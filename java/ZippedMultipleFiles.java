2a3e4169ce6588164e7acd4e6e4c7eada37a5ce1
Rafael Waterkemper <rafaelwaterkemper@gmail.com>
2019-01-30 14:44:50 -0200

    Add unzip method

--- java/ZippedMultipleFiles.java
+++ java/ZippedMultipleFiles.java
@@ -1,17 +1,20 @@
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.nio.file.Files;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZippedMultipleFiles {
 
     public static void main(String[] args) {
         geraArquivoZipado();
+        unzipArquivo("/home/rafael.waterkemper/temp/agoravai.zip");
     }
 
     static void geraArquivoZipado() {
-        String path = "C:/temp/agoravai.zip";
+        String path = "/home/rafael.waterkemper/temp/agoravai.zip";
         try {
             FileOutputStream fos = new FileOutputStream(path);
 
@@ -19,25 +22,42 @@ public class ZippedMultipleFiles {
 
             try {
                 for (int i = 0; i < 3; i++) {
-                    FileOutputStream fis = new FileOutputStream("arquivo" + i + ".txt");
-
-                    // begin writing a new ZIP entry, positions the stream to the start of the entry data
                     zos.putNextEntry(new ZipEntry("arquivo" + i + ".txt"));
-                    
+
                     zos.write(("Arquivo de testes " + i).getBytes());
 
                     zos.closeEntry();
-                    // close the InputStream
-                    fis.close();
                 }
-                // close the ZipOutputStream
                 zos.close();
             } catch (IOException ioe) {
                 System.out.println("Error creating zip file: " + ioe);
             }
-
         } catch (FileNotFoundException ee) {
             ee.printStackTrace();
+        }
+    }
+
+    static void unzipArquivo(String pathZip) {
+        ByteArrayInputStream fileZipped;
+        File arquivoZipado = new File(pathZip);
+
+        try {
+            fileZipped = new ByteArrayInputStream(Files.readAllBytes(arquivoZipado.toPath()));
+            ZipInputStream zis = new ZipInputStream(fileZipped);
+            ZipEntry zie = zis.getNextEntry();
+
+            while (zie != null) {
+                FileOutputStream fos = new FileOutputStream("/home/rafael.waterkemper/temp/" + zie.getName());
+
+                IOUtils.copy(zis, fos);
+                fos.close();
+                zie = zis.getNextEntry();
+            }
+            zis.closeEntry();
+            zis.close();
+            fileZipped.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
