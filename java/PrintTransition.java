d326d891791fc3d9805d562e19c0b4088552fb95
Rafael Waterkemper <rafaelwaterkemper@gmail.com>
2019-10-16 07:24:52 -0400

    Update PrintTransition.java

--- java/PrintTransition.java
+++ java/PrintTransition.java
@@ -1,5 +1,6 @@
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.zone.ZoneOffsetTransition;
 import java.time.zone.ZoneRules;
@@ -7,6 +8,12 @@ import java.time.zone.ZoneRules;
 public class PrintTransition {
 
     public static void main(String args[]) {
+        //Print default Timezone in according with OS timezone
+        //Ubuntu /etc/localtime
+        System.out.println(java.util.TimeZone.getDefault().getID());
+        
+        //Show time in according with timezone default
+        System.out.println(LocalDateTime.now());
         ZoneId zoneId = ZoneId.of("America/Sao_Paulo");
 
         ZoneRules rules = zoneId.getRules();
@@ -14,7 +21,7 @@ public class PrintTransition {
         LocalDate date = LocalDate.parse("2019-10-25");
         Instant instant = date.atStartOfDay(ZoneId.of("America/Sao_Paulo")).toInstant();
 
-        ZoneOffsetTransition nextTransition = rules.nextTransition(instant);
+        ZoneOffsetTransition nextTransition = rules.nextTransition(Instant.from(instant));
 
         System.out.println("Next transition at: " +
                 nextTransition.getInstant().atZone(zoneId));
